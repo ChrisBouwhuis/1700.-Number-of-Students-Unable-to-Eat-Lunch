df37988b5bd3a9f94faaee17f9644dbebcfbe7f4
Chris <0355882@student.rocvantwente.nl>
2024-04-08 22:16:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Classlib;
+
+namespace _1700._Number_of_Students_Unable_to_Eat_Lunch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] students = { 1, 1, 1, 0, 0, 1 };
+            int[] sandwiches = { 1, 0, 0, 0, 1, 1 };
+            int result = solution.CountStudents(students, sandwiches);
+            Console.WriteLine(result);
+        }
+    }
+}
